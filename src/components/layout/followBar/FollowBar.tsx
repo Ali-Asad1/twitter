0ffe17b126cc45b50594770f746d751d2e282125
Ali <aliasad.3128@gmail.com>
2023-09-24 01:23:14 +0330

    Create : followBar layout

---
+++ src/components/layout/followBar/FollowBar.tsx
@@ -0,0 +1,13 @@
+const Followbar = () => {
+  return (
+    <div className="px-6 py-4">
+      <div className="bg-slate-3 rounded-xl p-4">
+        <h2 className="font-semibold text-xl">Who to follow</h2>
+        <div className="flex flex-col gap-y-5 mt-4">
+          {/* Todo:  Follower list should be here */}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Followbar;
