0853f7fe608ea9f680ecdc3521bbf8c666b24e9b
Ali <aliasad.3128@gmail.com>
2023-09-25 23:31:34 +0330

    Change : using Button component

--- src/components/layout/sidebar/TweetButton.tsx
+++ src/components/layout/sidebar/TweetButton.tsx
@@ -1,15 +1,16 @@
+import Button from "@/components/common/Button";
 import { FaFeather } from "react-icons/fa";
 
 const TweetButton = () => {
   return (
-    <div className="select-none">
-      <button className="mt-6 w-14 h-14 p-4 flex lg:hidden justify-center items-center bg-blue-9 hover:bg-blue-10 active:bg-blue-11 rounded-full cursor-pointer transition">
-        <FaFeather size={24} color="white" />
-      </button>
-      <button className="w-full mt-6 hidden lg:block xp-6 py-2 bg-blue-9 hover:bg-blue-10 active:bg-blue-11 rounded-full cursor-pointer transition">
-        <p className="text-xl font-semibold text-white">Tweet</p>
-      </button>
-    </div>
+    <>
+      <Button btnWidth="full" className="hidden lg:block rounded-full !mt-6">
+        Tweet
+      </Button>
+      <Button btnType="iconOnly" btnSize="lg" className="block lg:hidden">
+        <FaFeather size={24} />
+      </Button>
+    </>
   );
 };
 export default TweetButton;
