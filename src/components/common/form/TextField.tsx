96d151ff10c5a797caa9f03279c8c536ce06a97b
Ali <aliasad.3128@gmail.com>
2023-09-26 15:43:53 +0330

    Create : textfield component

---
+++ src/components/common/form/TextField.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/utils/clsx";
+import { useField } from "formik";
+import { forwardRef } from "react";
+
+interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(({ ...props }, ref) => {
+  const [field, meta] = useField(props.name as string);
+
+  return (
+    <div className="w-full relative">
+      <input
+        ref={ref}
+        className={cn(
+          "peer w-full h-16 px-3 pt-3 bg-transparent border border-slate-6 rounded-md focus:border-blue-9 outline-none",
+          meta.touched && meta.error && "!border-red-9"
+        )}
+        {...field}
+        {...props}
+        autoComplete="off"
+      />
+      <label
+        htmlFor=""
+        className={cn(
+          "absolute top-[20px] left-[12px] text-base text-slate-11 peer-focus:-translate-y-3 peer-focus:text-xs peer-focus:text-blue-9 transition-all select-none pointer-events-none",
+          field.value.trim() && "-translate-y-3 text-xs",
+          meta.touched && meta.error && "peer-focus:text-red-9",
+          meta.error && field.value && "text-red-9"
+        )}
+      >
+        Username
+      </label>
+      {meta.touched && meta.error && (
+        <p className="absolute -bottom-5 left-[12px] text-sm text-red-9">{meta.error}</p>
+      )}
+    </div>
+  );
+});
+export default TextField;
