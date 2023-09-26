ade713c4f419c4ab2e412c740c7f1b027c5486c0
Ali <aliasad.3128@gmail.com>
2023-09-26 22:24:41 +0330

    Change : layout style

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,7 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div className="w-full h-full">{children}</div>;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full max-w-[560px] min-w-[300px] px-5">{children}</div>
+    </div>
+  );
 }
