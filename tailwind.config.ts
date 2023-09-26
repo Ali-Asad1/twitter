8e5110f4a09fa0fb3bbb53aaa478ccefeeda7e59
Ali <aliasad.3128@gmail.com>
2023-09-26 22:23:03 +0330

    Add : variable for fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        chirp: ["var(--font-chirp)"],
+        poppins: ["var(--font-poppins)"],
+        roboto: ["var(--font-roboto)"],
       },
     },
   },
