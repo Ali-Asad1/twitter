4ec5d36f0a5876b4b95dcd2d9bf487721cc95a7d
Ali <aliasad.3128@gmail.com>
2023-09-28 02:03:50 +0330

    Fix : error on request body

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -5,7 +5,7 @@ import prisma from "@/libs/prismadb";
 
 export async function POST(request: NextRequest) {
   try {
-    const { email, username, name, password }: any = request.body;
+    const { email, username, name, password }: any = await request.json();
     const hashedPassword = await bcrypt.hash(password, 12);
 
     const user = await prisma.user.create({
