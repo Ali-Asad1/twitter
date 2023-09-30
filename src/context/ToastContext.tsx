c69c1bf077c4337cee4e65194e9c2e6fb1d95258
Ali <aliasad.3128@gmail.com>
2023-10-01 01:46:08 +0330

    Change duration time for loading toast

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -3,6 +3,6 @@
 import { Toaster } from "react-hot-toast";
 
 const ToastContext = () => {
-  return <Toaster toastOptions={{ duration: 3000 }} />;
+  return <Toaster toastOptions={{ duration: 3000, loading: { duration: 50000 } }} />;
 };
 export default ToastContext;
