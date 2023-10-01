7f8fb0e6bc9c6829dba1035b5dca4537027c1eff
Ali <aliasad.3128@gmail.com>
2023-10-02 01:07:47 +0330

    Change style and add onclick function for button

--- src/components/layout/sidebar/SidebarItem.tsx
+++ src/components/layout/sidebar/SidebarItem.tsx
@@ -1,3 +1,6 @@
+"use client";
+import Button from "@/components/common/Button";
+import { useRouter } from "next/navigation";
 import { IconType } from "react-icons/";
 
 interface SidebarItemProps {
@@ -8,16 +11,36 @@ interface SidebarItemProps {
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({ label, icon: Icon, href, onClick }) => {
+  const { push } = useRouter();
+
+  const clickHandler = () => {
+    if (onClick) {
+      onClick();
+    } else if (href) {
+      push(href);
+    }
+  };
   return (
-    <div className="flex items-center select-none">
-      <div className="relative h-14 w-14 flex items-center justify-center p-4 rounded-full hover:bg-slate-4 active:bg-slate-5 cursor-pointer lg:hidden transition">
-        <Icon size={28} className="fill-slate-11" />
-      </div>
-      <div className="relative hidden w-full lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-4 active:bg-slate-5 cursor-pointer transition">
-        <Icon size={24} className="fill-slate-11" />
-        <p className="text-slate-12 text-xl">{label}</p>
-      </div>
-    </div>
+    <>
+      <Button
+        onClick={() => clickHandler()}
+        btnWidth="full"
+        btnStyle="tertiary"
+        className="hidden lg:flex justify-start gap-x-3 text-slate-12"
+      >
+        <Icon size={22} className="fill-slate-12" />
+        {label}
+      </Button>
+      <Button
+        onClick={() => clickHandler()}
+        btnSize="lg"
+        btnStyle="tertiary"
+        btnType="iconOnly"
+        className="flex lg:hidden"
+      >
+        <Icon size={22} className="fill-slate-12" />
+      </Button>
+    </>
   );
 };
 
