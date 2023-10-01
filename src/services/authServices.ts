885b0a6d05765ec7888679d1235237bfd0a0e02e
Ali <aliasad.3128@gmail.com>
2023-10-01 20:30:20 +0330

    Fix api url

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const registerUser = async (data: any) => {
-  return await axios.post("/register", data);
+  return await axios.post("/api/register", data);
 };
 
 export const getCurrentUser = async () => {
-  return await axios.get("/current");
+  return await axios.get("/api/current");
 };
