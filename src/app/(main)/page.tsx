4f05f56df82b570fc1b597d90dffa4bc96adc3a2
Ali <aliasad.3128@gmail.com>
2023-10-02 00:44:37 +0330

    Remove sign out button

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-import { signOut } from "next-auth/react";
-
 import Header from "@/components/header/Header";
 
 export default function Home() {
   return (
     <section className="col-span-2 lg:col-span-2 border-x border-slate-6">
       <Header lable="Home" />
-      <button onClick={() => signOut()}>signOut</button>
     </section>
   );
 }
