f5c2b49e822ff72f3454ae30f1ea8fdf959bd883
Ali <aliasad.3128@gmail.com>
2023-10-01 23:29:58 +0330

    Change getCurrentUser function

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -4,6 +4,6 @@ export const registerUser = async (data: any) => {
   return await axios.post("/api/register", data);
 };
 
-export const getCurrentUser = async () => {
-  return await axios.get("/api/current");
+export const getCurrentUser = () => {
+  return axios.get("/api/current").then((res) => res.data);
 };
