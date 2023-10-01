54cec619c1306d96adaabede65dccec2c635c889
Ali <aliasad.3128@gmail.com>
2023-10-02 01:10:06 +0330

    Add devtool to query client provider

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const QueryProvider = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools /> ;
+      </QueryClientProvider>
+    </>
+  );
 };
 export default QueryProvider;
