351d1d04fc4486a0e2079f1d6ac47be5eb52b6b5
Ali <aliasad.3128@gmail.com>
2023-10-02 01:08:22 +0330

    Change menu items by user authentication

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,40 +1,70 @@
-import { BsHouseFill, BsBellFill } from "react-icons/bs";
-import { BiLogOut } from "react-icons/bi";
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+
+import { BiLogIn, BiLogOut } from "react-icons/bi";
 import { FaUser } from "react-icons/fa";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
 
 import SidebarLogo from "./SidebarLogo";
 import SidebarItem from "./SidebarItem";
 import TweetButton from "./TweetButton";
+import SidebarLoadingItem from "./SidebarLoadingItem";
 
 export default function Sidebar() {
-  const items = [
-    {
-      label: "Home",
-      href: "/",
-      icon: BsHouseFill,
-    },
-    {
-      label: "Notifications",
-      href: "/notifications",
-      icon: BsBellFill,
-    },
-    {
-      label: "Profile",
-      href: "/users/test",
-      icon: FaUser,
-    },
-  ];
+  const items = {
+    authenticated: [
+      {
+        label: "Home",
+        href: "/",
+        icon: BsHouseFill,
+      },
+      {
+        label: "Notifications",
+        href: "/notifications",
+        icon: BsBellFill,
+      },
+      {
+        label: "Profile",
+        href: "/users/test",
+        icon: FaUser,
+      },
+    ],
+    unauthenticate: [
+      {
+        label: "Login",
+        href: "/login",
+        icon: BiLogIn,
+      },
+      {
+        label: "Create account",
+        href: "/register",
+        icon: FaUser,
+      },
+    ],
+  };
+
+  const session = useSession();
+
   return (
     <div className="col-span-1 h-full pr-6 py-4">
-      <div className="flex flex-col items-end lg:items-start">
-        <div className="space-y-2">
-          <SidebarLogo />
-          {items.map((item) => (
-            <SidebarItem key={item.href} {...item} />
-          ))}
-          <SidebarItem label="Logout" icon={BiLogOut} onClick={() => {}} />
-          <TweetButton />
-        </div>
+      <div className="flex flex-col items-end lg:items-start space-y-5">
+        <SidebarLogo />
+        {session.status === "loading" ? (
+          Array(4)
+            .fill(1)
+            .map((_, i) => <SidebarLoadingItem key={i} />)
+        ) : session.status === "authenticated" ? (
+          <>
+            {items.authenticated.map((item) => (
+              <SidebarItem key={item.href} {...item} />
+            ))}
+            <SidebarItem label="Logout" icon={BiLogOut} onClick={() => signOut()} />
+            <TweetButton />
+          </>
+        ) : (
+          items.unauthenticate.map((item) => <SidebarItem key={item.href} {...item} />)
+        )}
       </div>
     </div>
   );
