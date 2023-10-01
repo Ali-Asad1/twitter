e779c35bea8c2b8a0562055b3ad2caf48bd4b648
Ali <aliasad.3128@gmail.com>
2023-10-01 18:47:45 +0330

    Add get current user service

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -3,3 +3,7 @@ import apiConfig from "./config/axios";
 export const registerUser = (data: any) => {
   return apiConfig.post("/register", data);
 };
+
+export const getCurrentUser = () => {
+  return apiConfig.get("/current");
+};
