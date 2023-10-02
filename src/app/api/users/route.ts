6f99eece072829be6e81c88d26eb4e8c562e31ef
Ali <aliasad.3128@gmail.com>
2023-10-02 18:12:54 +0330

    Add get all user api

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,20 @@
+import prisma from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 10,
+      select: {
+        id: true,
+      },
+    });
+
+    return NextResponse.json(users, { status: 200 });
+  } catch (error: any) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
