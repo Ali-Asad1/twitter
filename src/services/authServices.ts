7dd57fdc3a857b2b8db485ebf7a5bc1526c1b7b8
Ali <aliasad.3128@gmail.com>
2023-10-02 18:13:19 +0330

    Remove user services

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -3,7 +3,3 @@ import axios from "axios";
 export const registerUser = async (data: any) => {
   return await axios.post("/api/register", data);
 };
-
-export const getCurrentUser = () => {
-  return axios.get("/api/current").then((res) => res.data);
-};
