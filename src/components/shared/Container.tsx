2eea2cb68b8dd77dd6f993b36a2a74f3ee6a7c70
Ali <aliasad.3128@gmail.com>
2023-10-02 22:41:28 +0330

    Change container style (full width on small size)

--- src/components/shared/Container.tsx
+++ src/components/shared/Container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="h-full container mx-auto px-5 lg:px-10">{children}</div>;
+  return <div className="w-full h-full xl:container xl:mx-auto px-5 lg:px-10">{children}</div>;
 }
