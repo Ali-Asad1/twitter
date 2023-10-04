0b2320a60630dad8bf7b3aeb37097be41cdd6bd0
Ali <aliasad.3128@gmail.com>
2023-10-04 18:50:37 +0330

    Add empty and error followers

--- src/components/layout/followBar/FollowBar.tsx
+++ src/components/layout/followBar/FollowBar.tsx
@@ -3,19 +3,23 @@
 import useAllUsers from "@/hooks/useAllUsers";
 import UserItem from "./UserItem";
 import UserItemLoading from "./UserItemLoading";
+import NotFoundFollowers from "./NotFoundFollowers";
 
 const Followbar = () => {
   const { data, error, isLoading } = useAllUsers();
   return (
     <div className="px-6 py-4">
       <div className="max-h-[400px] bg-slate-3 rounded-xl p-4 overflow-y-auto">
-        <h2 className="font-semibold text-xl">Who to follow</h2>
-        <div className="flex flex-col gap-y-5 mt-4">
-          {isLoading
-            ? Array(5)
-                .fill(1)
-                .map((_, i) => <UserItemLoading key={i} />)
-            : data.map((user: any) => <UserItem {...user} key={user.id} />)}
+        <div className="flex flex-col gap-y-5 ">
+          {isLoading ? (
+            Array(5)
+              .fill(1)
+              .map((_, i) => <UserItemLoading key={i} />)
+          ) : !data.length && error ? (
+            <NotFoundFollowers />
+          ) : (
+            data.map((user: any) => <UserItem {...user} key={user.id} />)
+          )}
         </div>
       </div>
     </div>
