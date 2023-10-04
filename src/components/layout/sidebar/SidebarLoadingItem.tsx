5efa129d74c829805c611779dd07f2e0cf882cec
Ali <aliasad.3128@gmail.com>
2023-10-04 18:51:07 +0330

    Chagen loading styles

--- src/components/layout/sidebar/SidebarLoadingItem.tsx
+++ src/components/layout/sidebar/SidebarLoadingItem.tsx
@@ -1,9 +1,9 @@
 const SidebarLoadingItem = () => {
   return (
     <>
-      <div className="hidden lg:flex w-full h-12 px-3 justify-start items-center gap-x-3 bg-slate-4 rounded-md animate-pulse">
-        <div className="w-5 h-5 bg-slate-6 rounded-sm"></div>
-        <div className="w-full h-5 bg-slate-6 rounded-md"></div>
+      <div className="hidden lg:flex w-full h-12 px-3 justify-start items-center gap-x-3 animate-pulse">
+        <div className="w-5 h-5 bg-slate-6 rounded-full"></div>
+        <div className="w-1/3 h-5 bg-slate-6 rounded-sm"></div>
       </div>
 
       <div className="flex lg:hidden w-14 h-14 justify-center items-center bg-slate-4 rounded-full animate-pulse">
