87a789d781dcf119150c6545e364df8c0e7bd26e
Ali <aliasad.3128@gmail.com>
2023-10-18 00:31:11 +0330

    Add edit button for profile

--- src/components/user/UserHero.tsx
+++ src/components/user/UserHero.tsx
@@ -3,32 +3,45 @@
 import Image from "next/image";
 
 import useUser from "@/hooks/useUser";
+
 import Avatar from "./Avatar";
+import EditBtn from "./EditBtn";
+import { useEditProfileModal } from "@/hooks/useEditProfileModal";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 const UserHero = ({ username }: { username: string }) => {
-  const { data, isLoading } = useUser(username);
+  const { data: user, isLoading } = useUser(username);
+  const { data: currentUser } = useCurrentUser();
 
+  const { onOpen } = useEditProfileModal();
   return (
     <div className="relative">
       <div className="w-full aspect-w-3 aspect-h-1 bg-slate-3">
-        {!isLoading && data?.coverImage && (
+        {!isLoading && user?.coverImage && (
           <Image
             fill
-            src={data?.coverImage as string}
+            src={user?.coverImage as string}
             alt="banner"
             objectFit="cover"
             placeholder="blur"
-            blurDataURL={data?.coverImage as string}
+            blurDataURL={user?.coverImage as string}
           />
         )}
       </div>
-      <Avatar
-        username={username}
-        src={data?.profileImage}
-        border="lg"
-        size="lg"
-        className="absolute bottom-0 left-5 translate-y-[50%]"
-      />
+      {currentUser?.id === user?.id && (
+        <div className="absolute z-10 right-2 bottom-2">
+          <EditBtn onClick={() => null} />
+        </div>
+      )}
+
+      <div className="absolute bottom-0 left-5 translate-y-[50%]">
+        {currentUser?.id === user?.id && (
+          <div className="absolute z-10 right-0 bottom-0">
+            <EditBtn onClick={onOpen} />
+          </div>
+        )}
+        <Avatar username={username} src={user?.profileImage} border="lg" size="lg" />
+      </div>
     </div>
   );
 };
