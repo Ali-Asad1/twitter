a4ac4cd3b879eb679256dea1a0d2c71a7e4304ef
Ali <aliasad.3128@gmail.com>
2023-10-17 19:07:14 +0330

    Create hook for edit profile modal states

---
+++ src/hooks/useEditProfileModal.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+interface EditProfileModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+export const useEditProfileModal = create<EditProfileModalStore>((set) => ({
+  isOpen: true,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
