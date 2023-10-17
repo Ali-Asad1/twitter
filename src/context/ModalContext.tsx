9bfbae9ebc26064191dfba1f86de5af5f93a169a
Ali <aliasad.3128@gmail.com>
2023-10-17 19:13:12 +0330

    Create modal context

---
+++ src/context/ModalContext.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+
+import EditProfileModal from "@/components/modal/EditModal/EditProfileModal";
+
+const ModalContext = () => {
+  return (
+      <EditProfileModal />
+  );
+};
+export default ModalContext;
