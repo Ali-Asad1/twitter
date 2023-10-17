fa271a4b43d90b11adbbcefb2a4b3684f8ab07a2
Ali <aliasad.3128@gmail.com>
2023-10-17 23:45:24 +0330

    Add update profile handler

--- src/components/modal/EditModal/EditProfileModal.tsx
+++ src/components/modal/EditModal/EditProfileModal.tsx
@@ -1,23 +1,47 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
+import toast from "react-hot-toast";
 
 import { useEditProfileModal } from "@/hooks/useEditProfileModal";
+import { useEditProfileImage } from "@/hooks/useEditUser";
 
-import ImageDropzone, { DropzoneOnDrop } from "@/components/imageDropzone/ImageDropzone";
+import ImageDropzone from "@/components/imageDropzone/ImageDropzone";
 import Modal from "../Modal";
 import Avatar from "@/components/user/Avatar";
 import Button from "@/components/common/Button";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 const EditProfileModal = () => {
-  const [fileImage, setFileImage] = useState<string | null>(null);
+  const [base64, setBase64] = useState<string | null | undefined>(null);
   const { isOpen, onClose } = useEditProfileModal();
+  const { data, isLoading } = useCurrentUser();
+  const { mutateAsync: updateAvatar } = useEditProfileImage();
 
-  const onDrop: DropzoneOnDrop = (file) => {
-    const preview = URL.createObjectURL(file[0]);
-    setFileImage(preview);
+  const updateSubmit = async () => {
+    const toastId = toast.loading("Updating profile ...");
+    updateAvatar(base64 || "")
+      .then(() => {
+        toast.success("Update successfuly", {
+          id: toastId,
+        });
+      })
+      .catch(() => {
+        toast.error("Update failure!", {
+          id: toastId,
+        });
+      })
+      .finally(() => {
+        onClose();
+      });
   };
+
+  useEffect(() => {
+    if (!isLoading) {
+      setBase64(data?.profileImage);
+    }
+  }, [isLoading]);
 
   return (
     <>
@@ -26,16 +50,21 @@ const EditProfileModal = () => {
           <Modal onClose={onClose}>
             <div className="w-full h-full pt-5 flex flex-col">
               <div className="flex justify-center mb-5">
-                <Avatar size="lg" src={fileImage || ""} username="" />
+                <Avatar size="lg" src={base64 || ""} username="" />
               </div>
               <div className="flex-1">
-                <ImageDropzone onDrop={onDrop} />
+                <ImageDropzone onChange={setBase64} />
               </div>
               <div className="flex justify-center gap-x-5 mt-5">
                 <Button btnWidth="full" btnStyle="outline" onClick={onClose}>
                   Cancel
                 </Button>
-                <Button btnWidth="full" btnStyle="primary">
+                <Button
+                  btnWidth="full"
+                  btnStyle="primary"
+                  onClick={updateSubmit}
+                  disabled={!base64}
+                >
                   Save
                 </Button>
               </div>
