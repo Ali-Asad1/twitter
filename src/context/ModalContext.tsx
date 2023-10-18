c701a1820d86502ba3064e9fe537c5de9b5c2403
Ali <aliasad.3128@gmail.com>
2023-10-18 22:48:37 +0330

    Add render conditions to context

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-
+import EditBannerModal from "@/components/modal/EditModal/EditBannerModal";
 import EditProfileModal from "@/components/modal/EditModal/EditProfileModal";
+import { useEditBannerModal } from "@/hooks/useEditBannerModal";
+import { useEditProfileModal } from "@/hooks/useEditProfileModal";
+import { AnimatePresence } from "framer-motion";
 
 const ModalContext = () => {
+  const { isOpen: isOpenBannedModal } = useEditBannerModal();
+  const { isOpen: isOpenProfileModal } = useEditProfileModal();
+
   return (
-      <EditProfileModal />
+    <AnimatePresence>
+      {isOpenProfileModal && <EditProfileModal />}
+      {isOpenBannedModal && <EditBannerModal />}
+    </AnimatePresence>
   );
 };
 export default ModalContext;
