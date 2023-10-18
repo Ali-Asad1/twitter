17c33c6d0a69a88c4f8364af850373577dae9151
Ali <aliasad.3128@gmail.com>
2023-10-18 23:13:24 +0330

    Add edit banner service

--- src/services/user.ts
+++ src/services/user.ts
@@ -15,3 +15,7 @@ export const getUser = async (username: string) => {
 export const editUserProfile = async (avatar: string) => {
   return await axios.patch("/api/user/edit/avatar", { avatar }).then((res) => res.data);
 };
+
+export const editUserBanner = async (banner: string) => {
+  return await axios.patch("/api/user/edit/banner", { banner }).then((res) => res.data);
+};
