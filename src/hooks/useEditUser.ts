61342b58d0350991f8e21db600ff2827c368ded1
Ali <aliasad.3128@gmail.com>
2023-10-18 23:13:52 +0330

    Add edit user mutation hook

--- src/hooks/useEditUser.ts
+++ src/hooks/useEditUser.ts
@@ -1,12 +1,25 @@
-import { editUserProfile } from "@/services/user";
+import { editUserBanner, editUserProfile } from "@/services/user";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useEditProfileImage = () => {
   const queryClient = useQueryClient();
-
   return useMutation(
     (avatar: string) => {
       return editUserProfile(avatar);
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(["user", data.username]);
+      },
+    }
+  );
+};
+
+export const useEditBannerImage = () => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    (banner: string) => {
+      return editUserBanner(banner);
     },
     {
       onSuccess: (data) => {
