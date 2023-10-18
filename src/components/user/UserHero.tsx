b19f39f1f069476cafdaab4e1f4d42e8ec2cd886
Ali <aliasad.3128@gmail.com>
2023-10-18 22:48:04 +0330

    Add onclick for edit icon buttons

--- src/components/user/UserHero.tsx
+++ src/components/user/UserHero.tsx
@@ -8,12 +8,15 @@ import Avatar from "./Avatar";
 import EditBtn from "./EditBtn";
 import { useEditProfileModal } from "@/hooks/useEditProfileModal";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import { useEditBannerModal } from "@/hooks/useEditBannerModal";
 
 const UserHero = ({ username }: { username: string }) => {
   const { data: user, isLoading } = useUser(username);
   const { data: currentUser } = useCurrentUser();
 
-  const { onOpen } = useEditProfileModal();
+  const { onOpen: onOpenProfileModal } = useEditProfileModal();
+  const { onOpen: onOpenBannerModal } = useEditBannerModal();
+
   return (
     <div className="relative">
       <div className="w-full aspect-w-3 aspect-h-1 bg-slate-3">
@@ -30,14 +33,14 @@ const UserHero = ({ username }: { username: string }) => {
       </div>
       {currentUser?.id === user?.id && (
         <div className="absolute z-10 right-2 bottom-2">
-          <EditBtn onClick={() => null} />
+          <EditBtn onClick={onOpenBannerModal} />
         </div>
       )}
 
       <div className="absolute bottom-0 left-5 translate-y-[50%]">
         {currentUser?.id === user?.id && (
           <div className="absolute z-10 right-0 bottom-0">
-            <EditBtn onClick={onOpen} />
+            <EditBtn onClick={onOpenProfileModal} />
           </div>
         )}
         <Avatar username={username} src={user?.profileImage} border="lg" size="lg" />
