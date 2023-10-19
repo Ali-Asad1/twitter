62267e42daf569d6e816b3d2bd2ef7d6f73b5991
Ali <aliasad.3128@gmail.com>
2023-10-19 23:58:04 +0330

    Add edit bio modal

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -1,19 +1,25 @@
 "use client";
 
-import EditBannerModal from "@/components/modal/EditModal/EditBannerModal";
-import EditProfileModal from "@/components/modal/EditModal/EditProfileModal";
-import { useEditBannerModal } from "@/hooks/useEditBannerModal";
-import { useEditProfileModal } from "@/hooks/useEditProfileModal";
 import { AnimatePresence } from "framer-motion";
+
+import { useEditBannerModal } from "@/hooks/useEditBannerModal";
+import { useEditBioModal } from "@/hooks/useEditBioModal";
+import { useEditProfileModal } from "@/hooks/useEditProfileModal";
+
+import EditBannerModal from "@/components/modal/EditModal/EditBannerModal";
+import EditBioModal from "@/components/modal/EditModal/EditBioModal";
+import EditProfileModal from "@/components/modal/EditModal/EditProfileModal";
 
 const ModalContext = () => {
   const { isOpen: isOpenBannedModal } = useEditBannerModal();
   const { isOpen: isOpenProfileModal } = useEditProfileModal();
+  const { isOpen: isOpenBioModal } = useEditBioModal();
 
   return (
     <AnimatePresence>
       {isOpenProfileModal && <EditProfileModal />}
       {isOpenBannedModal && <EditBannerModal />}
+      {isOpenBioModal && <EditBioModal />}
     </AnimatePresence>
   );
 };
