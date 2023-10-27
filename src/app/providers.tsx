893317ba9c6d4309f348ada626e23b4ea0ba68ab
Ali <aliasad.3128@gmail.com>
2023-10-27 20:27:48 +0330

    Add edgestore provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { EdgeStoreProvider } from "@/libs/edgeStore";
 import AuthProvider from "@/providers/AuthProvider";
 import QueryProvider from "@/providers/QueryProvider";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <AuthProvider>
-      <QueryProvider>{children}</QueryProvider>
+      <QueryProvider>
+        <EdgeStoreProvider>{children}</EdgeStoreProvider>
+      </QueryProvider>
     </AuthProvider>
   );
 };
