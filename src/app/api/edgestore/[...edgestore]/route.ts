5392f1dcaec6b6c075a6b101753e03e1f3c42b3f
Ali <aliasad.3128@gmail.com>
2023-10-27 23:19:51 +0330

    remove context

--- src/app/api/edgestore/[...edgestore]/route.ts
+++ src/app/api/edgestore/[...edgestore]/route.ts
@@ -1,23 +1,8 @@
-import getCurrentUser from "@/actions/getCurrentUser";
 import { initEdgeStore } from "@edgestore/server";
-import {
-  type CreateContextOptions,
-  createEdgeStoreNextHandler,
-} from "@edgestore/server/adapters/next/app";
+import { createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app";
 import { z } from "zod";
 
-type Context = {
-  username: string;
-};
-
-const createContext = async ({ req }: CreateContextOptions): Promise<Context> => {
-  const user = await getCurrentUser();
-  return {
-    username: user?.username as string,
-  };
-};
-
-const es = initEdgeStore.context<Context>().create();
+const es = initEdgeStore.create();
 
 const edgeStoreRouter = es.router({
   publicImages: es
@@ -25,14 +10,14 @@ const edgeStoreRouter = es.router({
     .input(
       z.object({
         category: z.string(),
+        username: z.string(),
       })
     )
-    .path(({ ctx, input }) => [{ category: input.category }, { author: ctx.username }]),
+    .path(({ input }) => [{ category: input.category }, { author: input.username }]),
 });
 
 const handler = createEdgeStoreNextHandler({
   router: edgeStoreRouter,
-  createContext,
 });
 
 export { handler as GET, handler as POST };
