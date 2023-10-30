12aed51d62fd04159eb756105c89c82797a90a55
Ali <aliasad.3128@gmail.com>
2023-10-30 22:56:05 +0330

    Add mutate post

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,12 +1,23 @@
-import { getAllPosts } from "@/services/post";
-import { PostType } from "@/types/post.type";
-import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+import { PostType } from "@/types/post.type";
+import { addNewPost, getAllPosts } from "@/services/post";
 
 export const usePosts = (userId?: string) => {
   const postUrl = userId ? `/api/posts?userId=${userId}` : "/api/posts";
   return useQuery<PostType[], AxiosError>({
-    queryKey: ["post", userId ? userId : "all"],
+    queryKey: ["posts", userId ? userId : "all"],
     queryFn: () => getAllPosts(postUrl),
+  });
+};
+
+export const useAddPost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation((body: string) => addNewPost(body), {
+    onSuccess() {
+      queryClient.invalidateQueries(["posts"]);
+    },
   });
 };
