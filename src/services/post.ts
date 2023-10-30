c00bd9726c0e55f8d8d0e27c20590ee744443b53
Ali <aliasad.3128@gmail.com>
2023-10-30 22:55:29 +0330

    Add new post service

--- src/services/post.ts
+++ src/services/post.ts
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getAllPosts = async (url: string) => {
   return await axios.get(url).then((res) => res.data);
 };
+
+export const addNewPost = async (body: string) => {
+  return await axios.post(`/api/posts`, { body }).then((res) => res.data);
+};
