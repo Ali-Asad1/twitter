5994ed5a5674b4f96c1db77187232b44cfb29e83
Ali <aliasad.3128@gmail.com>
2023-11-01 01:02:41 +0330

    Add api get one post data

---
+++ src/app/api/posts/[postId]/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/libs/prismadb";
+
+export async function GET(req: NextRequest, { params }: { params: { postId: string } }) {
+  const { postId: reqPostId } = params;
+
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: reqPostId,
+      },
+      include: {
+        comments: true,
+        user: {
+          select: {
+            name: true,
+            username: true,
+            profileImage: true,
+          },
+        },
+      },
+    });
+
+    if (!post) {
+      return new NextResponse("Not found", { status: 404 });
+    }
+
+    return NextResponse.json(post, { status: 200 });
+  } catch (err: unknown) {
+    console.log(err);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
