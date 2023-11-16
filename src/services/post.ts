2809c78bfce467ccb71fb06dfc8808811ed16fd9
Ali <aliasad.3128@gmail.com>
2023-11-17 00:33:37 +0330

    Add comment service

--- src/services/post.ts
+++ src/services/post.ts
@@ -11,3 +11,7 @@ export const getAllPosts = async (url: string) => {
 export const addNewPost = async (body: string) => {
   return await axios.post(`/api/posts`, { body }).then((res) => res.data);
 };
+
+export const addComment = async (body: string, postId: string) => {
+  return await axios.post(`/api/posts/comment?postId=${postId}`, { body }).then((res) => res.data);
+};
