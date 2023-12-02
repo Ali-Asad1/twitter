eb810eb410feb3ae29ad99d4d092005d72b76d7e
Ali <aliasad.3128@gmail.com>
2023-12-03 01:43:33 +0330

    Change edit btn component props

--- src/components/user/EditBtn.tsx
+++ src/components/user/EditBtn.tsx
@@ -4,11 +4,24 @@ import { BiPencil } from "react-icons/bi";
 
 import Button from "../common/Button";
 
-const EditBtn = ({ onClick }: { onClick: () => void }) => {
+type EditBtnProps = {
+  onClick: () => any;
+  btnType: "icon" | "default";
+};
+
+const EditBtn: React.FC<EditBtnProps> = ({ onClick, btnType }) => {
   return (
-    <Button onClick={onClick} btnSize="sm" btnType="iconOnly">
-      <BiPencil size={24} />
-    </Button>
+    <>
+      {btnType === "icon" ? (
+        <Button onClick={onClick} btnSize="sm" btnType="iconOnly">
+          <BiPencil size={24} />
+        </Button>
+      ) : (
+        <Button onClick={onClick} btnSize="sm" btnStyle="secondary">
+          Edit
+        </Button>
+      )}
+    </>
   );
 };
 export default EditBtn;
